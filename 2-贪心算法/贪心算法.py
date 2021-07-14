427873f45a410398351463bae7697dafbb900b6e
Gethin <gexu1990@hotmail.com>
2021-07-14 21:30:01 +0800

    change

--- 2-贪心算法/贪心算法.py
+++ 2-贪心算法/贪心算法.py
@@ -1,30 +1,27 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %% [markdown]
-#  # 贪心算法
-
-# %%
-from typing import List
+# # 贪心算法
 
 # %% [markdown]
-#  ##分配问题（find_content_children）
+# ##分配问题（find_content_children）
 
 # %% [markdown]
-#  ### 455. Assign Cookies (Easy)
-#  >有一群孩子和一堆饼干，每个孩子有一个饥饿度，每个饼干都有一个大小。每个孩子只能吃一个饼干，且只有饼干的大小不小于孩子的饥饿度时，这个孩子才能吃饱。求解最多有多少孩子可以吃饱。
+# ### 455. Assign Cookies (Easy)
+# >有一群孩子和一堆饼干，每个孩子有一个饥饿度，每个饼干都有一个大小。每个孩子只能吃一个饼干，且只有饼干的大小不小于孩子的饥饿度时，这个孩子才能吃饱。求解最多有多少孩子可以吃饱。
 
 # %%
 
 
-def find_content_children(children: List[int], cookies: list[int]):
+def find_content_children(children: list[int], cookies: list[int]):
     children.sort()
     cookies.sort()
-    child, cookie = [0, 0]
-    while(child < len(children) and cookie < len(cookies)):
-        if(children[child] <= cookies[cookie]):
-            child += 1
-            cookie += 1
-    return child
+    _child, _cookie = [0, 0]
+    while _child < len(children) and _cookie < len(cookies):
+        if children[_child] <= cookies[_cookie]:
+            _child += 1
+            _cookie += 1
+    return _child
 
 
 def find_content_children_test():
@@ -33,6 +30,7 @@ def find_content_children_test():
     result = find_content_children(children, cookies)
     print(result)
 
+
 # %% [markdown]
 #  ### 135. Candy (Hard)
 #  >一群孩子站成一排，每一个孩子有自己的评分。现在需要给这些孩子发糖果，规则是如果一个孩子的评分比自己身旁的一个孩子要高，那么这个孩子就必须得到比身旁孩子更多的糖果；所有孩子至少要有一个糖果。求解最少需要多少个糖果。
@@ -40,15 +38,15 @@ def find_content_children_test():
 # %%
 
 
-def candy(ratings: List[int]):
+def candy(ratings: list[int]):
     l = len(ratings)
     num = [1 for _ in range(l)]
-    for i in range(l):
-        if ratings[i] > ratings[i-1] and i-1 >= 0:
-            num[i] = num[i-1]+1
-    for i in range(l-1, -1, -1):
-        if ratings[i] < ratings[i-1] and i-1 >= 0:
-            num[i-1] = max(num[i-1], num[i]+1)
+    for _i in range(l):
+        if ratings[_i] > ratings[_i - 1] and _i - 1 >= 0:
+            num[_i] = num[_i - 1] + 1
+    for _i in range(l - 1, -1, -1):
+        if ratings[_i] < ratings[_i - 1] and _i - 1 >= 0:
+            num[_i - 1] = max(num[_i - 1], num[_i] + 1)
     return sum(num)
 
 
@@ -56,20 +54,22 @@ def candy_test():
     ratings = [1, 0, 2]
     result = candy(ratings)
     print(result)
+
+
 # %% [markdown]
 #  ### 435. Non-overlapping Intervals (Medium)
 #  >给定多个区间，计算让这些区间互不重叠所需要移除区间的最少个数。起止相连不算重叠。
 
 
-def erase_overlap_intervals(intervals: List[List[int]]) -> int:
+def erase_overlap_intervals(intervals: list[list[int]]) -> int:
     delete = 0
-    intervals.sort(lambda a: a[1])
+    intervals.sort(key=lambda a: a[1])
     right = intervals[0][1]
-    for i in range(1, len(intervals)):
-        if right > intervals[i][0]:
+    for _i in range(1, len(intervals)):
+        if right > intervals[_i][0]:
             delete += 1
         else:
-            right = intervals[i][1]
+            right = intervals[_i][1]
     return delete
 
 
@@ -80,4 +80,6 @@ def erase_overlap_intervals_test():
 
 
 # %%
+find_content_children_test()
+candy_test()
 erase_overlap_intervals_test()
